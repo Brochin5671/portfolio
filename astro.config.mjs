ad5c713b68f5568c21f8e5272bd5b985dac56290
Maxim Brochin <maximbrochin@gmail.com>
2023-01-30 00:24:02 -0500

    Added tailwind and some basic styling for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
-
 // https://astro.build/config
 import solidJs from "@astrojs/solid-js";
-
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
+  integrations: [solidJs(), tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
---
+++ src/components/Experience.jsx
@@ -0,0 +1,8 @@
+export default function Experience({ jobTitle, company }) {
+  return (
+    <>
+      <h5 class="text-2xl text-center my-2">{jobTitle}</h5>
+      <p class="text-center">{company}</p>
+    </>
+  );
+}
