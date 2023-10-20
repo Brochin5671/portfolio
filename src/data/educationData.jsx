d72ea345b610e8c16c5d005bd17c01527e9c779d
Brochin5671 <maximbrochin@gmail.com>
2023-10-20 18:12:16 -0400

    Add data for exp and projects

---
+++ src/data/educationData.jsx
@@ -0,0 +1,10 @@
+import uofgLogo from "../assets/uofg_logo.png"
+
+export const educationData = [
+  {
+    title: "Bachelor of Computing, Computer Science (Co-op) ",
+    subtitle: "University of Guelph",
+    dateRange: "Sep. 2019 - May 2024",
+    imgsrc: uofgLogo,
+  },
+]
---
+++ src/data/experienceData.jsx
@@ -0,0 +1,31 @@
+import clearLogo from "../assets/clear_logo.jpg"
+import tdLogo from "../assets/TD_logo.svg"
+import ncrLogo from "../assets/NCR_logo.svg"
+import uofgLogo from "../assets/uofg_logo.png"
+
+export const experienceData = [
+  {
+    title: "Software Developer",
+    subtitle: "Clear Software Inc.",
+    dateRange: "Jan. 2023 - Jul. 2023",
+    imgsrc: clearLogo,
+  },
+  {
+    title: "Software Engineer",
+    subtitle: "TD",
+    dateRange: "May 2022 - Aug. 2022",
+    imgsrc: tdLogo,
+  },
+  {
+    title: "Software Developer",
+    subtitle: "NCR",
+    dateRange: "Sep. 2021 - Dec. 2021",
+    imgsrc: ncrLogo,
+  },
+  {
+    title: "Data Programmer and Analyst",
+    subtitle: "University of Guelph",
+    dateRange: "Jun. 2021 - Sep. 2021",
+    imgsrc: uofgLogo,
+  },
+]
