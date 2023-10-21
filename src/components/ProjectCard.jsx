700546e0f59b5e1cb06780e5a4b70cbfa8e0f75f
Brochin5671 <maximbrochin@gmail.com>
2023-10-20 21:52:08 -0400

    Add project cards

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,47 @@
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material"
+import GitHubIcon from "@mui/icons-material/GitHub"
+import LinkIcon from "@mui/icons-material/Link"
+
+export function ProjectCard({ data }) {
+  const { name, desc, srcLink, link, imgsrc } = data
+
+  return (
+    <Grid>
+      <Card sx={{ maxWidth: 345, margin: "1em" }}>
+        <CardMedia component="img" alt={name} height="140" image={imgsrc} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {desc}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          {srcLink && (
+            <IconButton
+              href={srcLink}
+              target="_blank"
+              aria-label="Source code link"
+            >
+              <GitHubIcon />
+            </IconButton>
+          )}
+          {link && (
+            <IconButton href={link} target="_blank" aria-label="Website link">
+              <LinkIcon />
+            </IconButton>
+          )}
+        </CardActions>
+      </Card>
+    </Grid>
+  )
+}
