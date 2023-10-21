9c1a65c494954d61de84ddef1b70c707305882e8
Brochin5671 <maximbrochin@gmail.com>
2023-10-20 23:00:11 -0400

    Update some packages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import preact from "@preact/preset-vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Supress warning: https://stackoverflow.com/questions/76694615/module-level-directives-cause-errors-when-bundled-use-client-was-ignored-caus
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return
+        }
+        warn(warning)
+      },
+    },
+  },
   plugins: [preact()],
 })
