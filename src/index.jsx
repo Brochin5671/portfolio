0c212434725c6d49eaee97b121d8f6704349e498
Brochin5671 <maximbrochin@gmail.com>
2023-10-23 22:58:31 -0400

    About content and dark theme

--- src/index.jsx
+++ src/index.jsx
@@ -3,38 +3,88 @@
 
 import { render } from "preact"
 import "./style.css"
-import { Box, Container, Divider, Grid, Typography } from "@mui/material"
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material"
 import { experienceData } from "./data/experienceData"
 import { educationData } from "./data/educationData"
 import { ItemList } from "./components/ItemList"
 import { ProjectCard } from "./components/ProjectCard"
 import { projectData } from "./data/projectData"
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+})
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <Container>
-        <h1>Hey, I'm Max👋</h1>
+        <Typography variant="h1" marginBottom={2}>
+          Hey, I'm Max👋
+        </Typography>
         <Divider />
         <section>
           <h2>About Me</h2>
+          <Grid container justifyContent="center">
+            <Card sx={{ maxWidth: 250, margin: "1em", marginBottom: "2em" }}>
+              <CardMedia
+                component="img"
+                alt={name}
+                loading="lazy"
+                image="/src/assets/about/me.jpg"
+              />
+            </Card>
+          </Grid>
           <Typography>
             I attend the University of Guelph for the Computer Science Co-op
             program, finishing off my 5th and last year!
           </Typography>
           <br />
           <Typography>
-            I have a good amount of experience with web-development, languages
-            and technologies include: JavaScript, TypeScript, React, HTML/CSS,
-            GraphQL, SQL, MongoDB, Express, Node and Docker. I also have
-            experience with Python, C and Java through personal projects and
-            coding challenges.
+            I had the privelege of working at several companies demonstrating a
+            variety of technical knowledge and skills. I have a good amount of
+            experience with web-development; most languages and technologies
+            include: JavaScript, TypeScript, React, HTML/CSS, GraphQL, SQL,
+            MongoDB, Express, Node and Docker. I also have experience with
+            Python, C and Java through personal projects and coding challenges.
           </Typography>
           <br />
           <Typography>
             My favorite hobby is Astrophotography, really beautiful what sights
             reside in the night-sky!
           </Typography>
+          <Grid container justifyContent="center">
+            <Card sx={{ maxWidth: 250, margin: "1em" }}>
+              <CardMedia
+                component="img"
+                alt="Jupiter rotating"
+                loading="lazy"
+                image="/src/assets/about/jupiter.gif"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Jupiter Rotation
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Captured with my 8" SCT and dedicated astrocam, rotation takes
+                  place over 41 minutes.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
           <br />
         </section>
         <Divider />
@@ -63,12 +113,14 @@ export function App() {
       </Container>
       <Divider sx={{ marginTop: "2em" }} variant="middle" />
       <footer>
-        <Typography variant="subtitle2" padding={2} textAlign="center">
+        <Typography variant="subtitle1" paddingTop={2} textAlign="center">
           © 2023 Maxim Brochin
         </Typography>
+        <Typography variant="subtitle2" padding={2} textAlign="center">
+          maximbrochin@gmail.com
+        </Typography>
       </footer>
-      <Divider sx={{ marginBottom: "2em" }} variant="middle" />
-    </>
+    </ThemeProvider>
   )
 }
 
